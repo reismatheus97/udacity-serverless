2231e8386fc37ec0ddcaaa738ce57462ac249e49
MatheusReis <matheus.reis@efabrika.com.br>
2020-04-29 19:11:15 -0300

    Updated config file

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'lcy9hiutng'
+const apiId = 'fyyac7ukqf'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
